b976be3c2a754bc6433c6948a000804a2d85f43e
SixExtreme <943459049@qq.com>
2019-04-10 18:51:29 +0800

    fix: review 144

--- binary_tree/traversal/144_binary_tree_preorder_traversal.py
+++ binary_tree/traversal/144_binary_tree_preorder_traversal.py
@@ -97,6 +97,14 @@ def test_solution():
     root = TreeNode(1)
     root.right = TreeNode(2)
     root.right.left = TreeNode(3)
-
     assert Solution().preorderTraversal(root) == [1, 2, 3]
+
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    assert Solution().preorderTraversal(root) == [1, 2, 4, 5, 3, 6, 7]
 
